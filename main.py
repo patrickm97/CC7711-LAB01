3cbbb82a161c5f14d068e25ab2778ca16c117a23
Patrick <43248624+patrickm97@users.noreply.github.com>
2023-03-05 20:18:15 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from chatbot import ChatBot
+myChatBot = ChatBot()
+#apenas carregar um modelo pronto
+myChatBot.loadModel()
+
+#criar o modelo
+myChatBot.createModel()
+
+print("Bem vindo ao Chatbot")
+
+pergunta = input("como posso te ajudar?\n")
+resposta, intencao = myChatBot.chatbot_response(pergunta)
+print(resposta + "   ["+intencao[0]['intent']+"]")
+
+while (intencao[0]['intent']!="despedida"):
+    pergunta = input("posso lhe ajudar com algo a mais?\n")
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+    print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+print("Foi um prazer atendê-lo")
